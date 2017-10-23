c343f00b73c9bd3746307a6b84a06bbfb6008c99
iTzDreamZ <31861247+Siddthecoder35@users.noreply.github.com>
2017-10-23 09:06:30 -0700

    made the harold ball give random numbers

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//create a list of strings, to hold harold ball responses
+var list = ["yaaas",
+"Nah",
+"Hell Yea Dude",
+"Sorry I forgot the question try again",
+"just google it",
+"lol idk bro",
+"thats kinda a weird question",
+"great question",
+"error 404 response not found"];
+
+//this function outputs a random number
+function randomNum(){
+	return Math.floor(Math.random() * 10);
+}
 //this functions responds with a yes or no to answer a yes or no question
 function shakeMagicharoldBall(){
 	//alert("test");
@@ -6,6 +21,8 @@ function shakeMagicharoldBall(){
 	document.getElementById("magicharoldball").src="Images/happyharold.jpg";
 
 	//grab the results
-	document.getElementById("results").innerHTML="yaaas"
+	document.getElementById("results").innerHTML=list[randomNum()];
 
-}
+//alert(randomNum()); 
+
+} 
