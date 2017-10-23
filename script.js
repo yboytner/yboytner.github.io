da3b303ea9bea8fd1748e891f05d12216e9747e8
iTzDreamZ <31861247+Siddthecoder35@users.noreply.github.com>
2017-10-23 08:12:13 -0700

    I fixed everything

---
+++ script.js
@@ -0,0 +1,11 @@
+//this functions responds with a yes or no anseert to a yes or no question
+function shakeMagicharoldBall(){
+	//alert("test");
+
+	//grab image by id
+	document.getElementById("magicharoldball").src="Images/happyharold.jpg";
+
+	//grab the results
+	document.getElementById("results").innerHTML="yaaas"
+
+}
