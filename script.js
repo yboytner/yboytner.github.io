7df5436b947682423fabcc500036cbcf215f36ac
iTzDreamZ <31861247+Siddthecoder35@users.noreply.github.com>
2017-10-23 09:50:49 -0700

    made harold way cooler

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 //create a list of strings, to hold harold ball responses
 var list = ["yaaas",
-"Nah",
-"Hell Yea Dude",
-"Sorry I forgot the question try again",
-"just google it",
-"lol idk bro",
-"thats kinda a weird question",
-"great question",
-"error 404 response not found"];
+			"Nah",
+			"Hell Yea Dude",
+			"Sorry I forgot the question try again",
+			"just google it",
+			"lol idk bro",
+			"thats kinda a weird question",
+			"great question",
+			"error 404 response not found",
+			"jee wiz man I dont know"];
+
+var imageState = "front";
 
 //this function outputs a random number
 function randomNum(){
@@ -15,13 +18,32 @@ function randomNum(){
 }
 //this functions responds with a yes or no to answer a yes or no question
 function shakeMagicharoldBall(){
-	//alert("test");
+	
+	//if the picture is the front side...
+	if(imageState == "front"){
+		document.getElementById("magicharoldball").src == "images/happyharold.jpg"
 
-	//grab image by id
-	document.getElementById("magicharoldball").src="Images/happyharold.jpg";
+		//grab image by id
+		document.getElementById("magicharoldball").src="Images/happyharold.jpg";
 
-	//grab the results
-	document.getElementById("results").innerHTML=list[randomNum()];
+		//grab the results
+		document.getElementById("results").innerHTML=list[randomNum()];
+		
+		imageState="back";
+}
+	//else, it's the pictures back side...
+	else{
+		//go back to the front picture
+		document.getElementById("magicharoldball").src="images/harold.jpg";
+
+		//change results paragraph to say something to prompt the user
+		//to shake harold again
+		document.getElementById("results").innerHTML="Click the Magic Harold to answer your yes or no question";
+
+		imageState = "front";
+}
+
+
 
 //alert(randomNum()); 
 
