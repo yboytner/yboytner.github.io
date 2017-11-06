0881c12440fe40e61b0b22fe6378493abac0644a
Wyeth Summerford-Ng <wsummerford@newroads.org>
2017-11-06 09:37:44 -0800

    SO MANY CHANGES

--- tictactoescript.js
+++ tictactoescript.js
@@ -1,7 +1,14 @@
 var character = 'X';
 
+
 //this function handles onclick event
 function turn(location){
+
+	//update box with character if its empty
+	if(document.getElementById(location).innerHTML == ""){
+
+		//update results div with feedback
+	document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
 
 	//switch between X and O
 	if(character == 'X'){
@@ -10,5 +17,10 @@ function turn(location){
 	else{
 		character = 'X';
 	}
-	document.getElementById(location).innerHTML = character;
+
+
+		//write character in box
+		document.getElementById(location).innerHTML = character;
+	}
+
 }
