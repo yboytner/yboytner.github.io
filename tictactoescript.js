4cce95b3721aefc7fca3bbe11450ca90b6279e7a
Johns <csen@newroads.org>
2017-11-13 09:35:41 -0800

    fixed tic tac toe

--- tictactoescript.js
+++ tictactoescript.js
@@ -3,9 +3,13 @@ var character = 'X';
 
 //this function handles onclick event
 function turn(location){
-<<<<<<< HEAD
+	
 	//update the box with character, if box is empty
 	if(document.getElementById(location).innerHTML == ""){
+		
+		//update results div with feedback
+	document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
+
 		//switch between X and O
 		if(character == 'X'){
 			character = 'O';
@@ -13,22 +17,10 @@ function turn(location){
 		else{
 			character = 'X';
 		}
-		//write character in a box
+	
+	//write character in box
 		document.getElementById(location).innerHTML = character;
-		//check for winner after move
 		checkWinner();
-=======
-
-	//update box with character if its empty
-	if(document.getElementById(location).innerHTML == ""){
-
-		//update results div with feedback
-	document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
-
-	//switch between X and O
-	if(character == 'X'){
-		character = 'O';
->>>>>>> 0881c12440fe40e61b0b22fe6378493abac0644a
 	}
 }
 
@@ -47,17 +39,13 @@ function checkWinner(){
 	var r3c3 = document.getElementById("r3c3").innerHTML;
 
 	if(((r1c1 == r1c2) && (r1c1 == r1c3) && r1c1 != '') ||
-	  ((r2c1 == r2c2) && (r2c1 == r2c3) && r2c1 !='') ||
-	  (r3c1 == r3c2) && (r3c1 == r3c3) && r3c1 !=''){
-		alert("Winner!");
-	}
+	   ((r2c1 == r2c2) && (r2c1 == r2c3) && r2c1 != '') ||
+	   ((r3c1 == r3c2) && (r3c1 == r3c3) && r3c1 != '') ||
+	   ((r1c1 == r2c1) && (r1c1 == r3c1) && r1c1 != '') ||
+	   ((r1c2 == r2c2) && (r1c2 == r3c2) && r1c2 != '') ||
+	   ((r1c3 == r2c3) && (r1c3 == r3c3) && r1c3 != '') ||
+	   ((r1c1 == r2c2) && (r1c1 == r3c3) && r1c1 != '') ||
+	   ((r1c3 == r2c2) && (r1c3 == r3c1) && r1c3 != '')){
+	//alert ("winner winner chicken dinner!A")}
 
-<<<<<<< HEAD
-=======
-
-		//write character in box
-		document.getElementById(location).innerHTML = character;
-	}
-
->>>>>>> 0881c12440fe40e61b0b22fe6378493abac0644a
 }
