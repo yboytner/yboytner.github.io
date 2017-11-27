559e8847b5cd504725fb363fc43259d6e6213bbc
Johns <csen@newroads.org>
2017-11-27 09:49:19 -0800

    began programing the circle

--- Clickable.js
+++ Clickable.js
@@ -1,4 +1,24 @@
 //draws random circle in clickPanel
 function randomCircle(){
 	
+	//randomly get top positions
+	document/getElementById(circle).style.top = "100px";
+	//randomly get left positions
+	document/getElementById(circle).style.left = "100px";
+	//update position of circle
+
+
+}
+
+//handles onclick event
+
+//when a random circle is click in clickPanel, it dissapears
+//then generates a new random circle
+function clickCircle(){
+
+	//circle is clicked
+	document.getElementById('circle').style.visibility = "hidden";	
+
+	//generates a random circle
+
 }
