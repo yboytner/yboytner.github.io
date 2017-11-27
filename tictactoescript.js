a73a9957d24819a3c7226572f76134820529d411
Johns <csen@newroads.org>
2017-11-27 08:21:33 -0800

    programmed reset button

--- tictactoescript.js
+++ tictactoescript.js
@@ -1,4 +1,4 @@
-var character = 'X';
+		var character = 'X';
 
 
 //this function handles onclick event
@@ -51,4 +51,17 @@ function checkWinner(){
 	   //update the results div
 	   document.getElementById("results").innerHTML = character + " wins!";
 	}
+}
+
+//clear tic tac toe board
+function clearAll(){
+		document.getElementById("r1c1").innerHTML="";
+		document.getElementById("r1c2").innerHTML="";
+		document.getElementById("r1c3").innerHTML="";
+		document.getElementById("r2c1").innerHTML="";
+		document.getElementById("r2c2").innerHTML="";
+		document.getElementById("r2c3").innerHTML="";
+		document.getElementById("r3c1").innerHTML="";
+		document.getElementById("r3c2").innerHTML="";
+		document.getElementById("r3c3").innerHTML="";
 }
